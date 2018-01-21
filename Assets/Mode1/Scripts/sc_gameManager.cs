69f0ccfc1250e78a16b874ee2ccd8e880ffe7bf4
antoine <antoine.rato@gmail.com>
2018-01-21 20:57:23 +0100

    Menu creation & graphic initialisation
    • Creation of the Menu
    • Creation of the mode1 graphics :
    - Initisialisation of the visual part

---
+++ Assets/Mode1/Scripts/sc_gameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_gameManager : MonoBehaviour
+{
+    public GameObject player1, player2;
+    public TextMesh textCoin;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+
+    public void OnMouseDown()
+    {
+        if(this.name == "up_arrow")
+        {
+            up_arrow();
+        }
+        else if(this.name == "down_arrow")
+        {
+            down_arrow();
+        }
+    }
+
+    public void up_arrow()
+    {
+        if(int.Parse(textCoin.text) < 10)
+            textCoin.text = "" + (int.Parse(textCoin.text) + 1);
+    }
+
+    public void down_arrow()
+    {
+        if (int.Parse(textCoin.text) > 0)
+            textCoin.text = "" + (int.Parse(textCoin.text) - 1);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
